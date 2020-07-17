7b0495c4f46655fe32f78fe7cc932d65c55686a4
Le Van Duc <duclv@kdb.snu.ac.kr>
2020-07-17 16:03:58 +0900

    First commit

---
+++ data/sensor_graph/cal_distance.py
@@ -0,0 +1,24 @@
+import os
+import geopy.distance
+
+newfile = open('distances.csv', 'w')
+newfile.write('from,to,cost\n')
+station_ids, lats, lons = [], [], []
+with open('pm25_stations.csv', 'r') as f:
+    for row in f:
+        cols = row.split(',')
+        station_ids.append(cols[1])
+        lons.append(float(cols[2]))
+        lats.append(float(cols[3]))
+
+# calculate distance
+for i in range(len(station_ids)):
+    for j in range(len(station_ids)):
+        s_from = station_ids[i]
+        s_to = station_ids[j]
+        cord1 = (lats[i], lons[i])
+        cord2 = (lats[j], lons[j])
+        d = int(geopy.distance.distance(cord1, cord2).km * 1000)
+        newfile.write(s_from + ',' + s_to + ',' + str(d) + '\n')
+newfile.flush()
+newfile.close()
